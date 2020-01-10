9a9c4d8e71f77df94b20b49a2b1e5424e8ee91d5
Chris Ling <lingmanhau@hotmail.com>
2020-01-10 19:47:11 +0100

    FolderStructures

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import styles from './src/styles/Theme';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <ThemeProvider theme={styles}>
+      <View style={{flex: 1, backgroundColor: styles.lightGreyColor}}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    </ThemeProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/styles/Theme.js
@@ -0,0 +1,8 @@
+export default { 
+    pinkColor: '#FF6F5E',
+    greenColor: '#23F0A2',
+    darkBlueColor: '#2C3D63',
+    lightGreyColor: '#F1F3F6',
+    darkGreyColor: '#888888',
+    blackColor: '#121316',
+}
