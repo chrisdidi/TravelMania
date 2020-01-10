4d26d20cb2261594b71ba964369b6c85415f19fb
Chris Ling <lingmanhau@hotmail.com>
2020-01-10 20:55:02 +0100

    Screens

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { ThemeProvider } from 'styled-components';
+import StackNavigation from './src/navigations/StackNavigations';
 import styles from './src/styles/Theme';
 
 export default function App() {
   return (
     <ThemeProvider theme={styles}>
       <View style={{flex: 1, backgroundColor: styles.lightGreyColor}}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <StackNavigation />
       </View>
     </ThemeProvider>
   );
---
+++ src/screens/ItineraryList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+import styled from 'styled-components';
+
+const Wrapper = styled.View`
+
+`;
+
+export default () => {
+    return(
+        <Wrapper>
+
+        </Wrapper>
+    )
+}
