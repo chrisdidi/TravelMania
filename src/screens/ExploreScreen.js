43907706f7cd90dfcf3f76bb0732413737130913
Martynas Narijauskas <marcius136@gmail.com>
2020-01-20 18:39:16 +0200

    Optimised code with google search

--- src/screens/ExploreScreen.js
+++ src/screens/ExploreScreen.js
@@ -8,9 +8,9 @@ import ContextCreator from '../context/ContextCreator';
 import styles from '../styles/Theme';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import MapView, { Marker, Callout } from 'react-native-maps';
-import Drawer from '../components/ExploreScreen/Drawer'
+import Drawer from '../components/ExploreScreen/Drawer';
 import AttractionCard from '../components/ExploreScreen/AttractionCard';
-import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import GoogleTextInput from '../components/GoogleTextInput';
 
 const Wrapper = styled.View`
     flex: 1;
@@ -103,87 +103,11 @@ export default class ExploreScreen extends Component {
                                     })}
                                 </MapView>
                                 <SearchBarWrapper>
-                                    <GooglePlacesAutocomplete
-                                        placeholder="Try Paris"
-                                        minLength={2} // minimum length of text to search
-                                        autoFocus={false}
-                                        fetchDetails={true}
-                                        onPress={async (data, details = null) => { // 'details' is provided when fetchDetails = true
-                                            let latLng = await context.getLatLngFromGoogle(data.place_id)
-                                            context.updateCurrentLocation(latLng)
-                                            await context.searchNearbyAttractions(latLng)
-                                            this.setState({
-                                                showList: false
-                                            })
-                                        }}
-                                        listViewDisplayed={this.state.showList}
-                                        getDefaultValue={() => {
-                                            return ''; // text input default value
-                                        }}
-                                        query={{
-                                            // available options: https://developers.google.com/places/web-service/autocomplete
-                                            key: 'AIzaSyCgQNpZtDRvveEeDUkVBOrzy-TcV1QWbMU',
-                                            language: 'en', // language of the results
-                                        }}
-                                        styles={{
-                                            description: {
-                                                fontWeight: 'bold',
-                                                color: '#000',
-                                                fontSize: 16
-                                            },
-                                            textInputContainer: {
-                                                width: constants.width - 60,
-                                                backgroundColor: '#fff',
-                                                borderTopWidth: 0,
-                                                borderBottomWidth: 0
-                                            },
-                                            predefinedPlacesDescription: {
-                                                color: '#1faadb',
-                                            },
-                                            listView: {
-                                                backgroundColor: '#fff',
-                                                width: constants.width - 60,
-                                                padding: 5,
-                                                height: '100%'
-                                            },
-                                            container: {
-                                                backgroundColor: '#fff',
-                                                borderRadius: 12,
-                                                paddingLeft: 10,
-                                                paddingRight: 10,
-                                                elevation: 5,
-                                                shadowColor: '#000000',
-                                                shadowOffset: { width: 0, height: 3 },
-                                                shadowOpacity: 0.2,
-                                                zIndex: 3,
-                                            },
-                                            powered: {
-                                                display: 'none',
-                                                height: 0
-                                            },
-                                            poweredContainer: {
-                                                padding: 0,
-                                                height: 0
-                                            }
-                                        }}
-
-                                        nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
-                                        GoogleReverseGeocodingQuery={{
-                                            // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
-                                        }}
-                                        GooglePlacesSearchQuery={{
-                                            // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
-                                            rankby: 'distance',
-                                            types: 'food',
-                                        }}
-                                        GooglePlacesDetailsQuery={{
-                                            // available options for GooglePlacesDetails API : https://developers.google.com/places/web-service/details
-                                            fields: 'formatted_address',
-                                        }}
-
-                                        filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
-                                        renderLeftButton={context.searchLoading ? () => <ActivityIndicator /> : () => <View />}
-                                    />
+                                    <GoogleTextInput onPressEvent={async (data) => {
+                                        let latLng = await context.getLatLngFromGoogle(data.place_id)
+                                        context.updateCurrentLocation(latLng)
+                                        await context.searchNearbyAttractions(latLng)
+                                    }} />
                                 </SearchBarWrapper>
                             </>
                         )
--- src/screens/OptimisationScreen.js
+++ src/screens/OptimisationScreen.js
@@ -1,11 +1,9 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext } from 'react';
 import styled from 'styled-components';
-import { Text, View } from 'react-native'
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { View } from 'react-native'
 import PrimaryButton from '../components/PrimaryButton'
-import constants from '../constants';
 import ContextCreator from '../context/ContextCreator'
-import Constants from 'expo-constants';
+import GoogleTextInput from '../components/GoogleTextInput'
 
 const Wrapper = styled.View`
 `;
@@ -31,157 +29,16 @@ export default (props) => {
         }}>
             <View style={{ marginTop: 10, width: "100%", height: "100%", backgroundColor: "#2C3D63", borderTopLeftRadius: 10, borderTopRightRadius: 10 }}>
                 <SearchBarWrapper>
-                    <GooglePlacesAutocomplete
-                        minLength={2} // minimum length of text to search
-                        autoFocus={false}
-                        fetchDetails={true}
-                        onPress={async (data, details = null) => { // 'details' is provided when fetchDetails = true
-                            let latLng = await getLatLngFromGoogle(data.place_id)
-                            setBeginPoint(latLng)
-                        }}
-                        getDefaultValue={() => {
-                            return ''; // text input default value
-                        }}
-                        query={{
-                            // available options: https://developers.google.com/places/web-service/autocomplete
-                            key: 'AIzaSyCgQNpZtDRvveEeDUkVBOrzy-TcV1QWbMU',
-                            language: 'en', // language of the results
-                        }}
-                        styles={{
-                            description: {
-                                fontWeight: 'bold',
-                                color: '#000',
-                                fontSize: 16
-                            },
-                            textInputContainer: {
-                                width: constants.width - 60,
-                                backgroundColor: '#fff',
-                                borderTopWidth: 0,
-                                borderBottomWidth: 0
-                            },
-                            predefinedPlacesDescription: {
-                                color: '#1faadb',
-                            },
-                            listView: {
-                                backgroundColor: '#fff',
-                                width: constants.width - 60,
-                                padding: 5,
-                                height: '100%'
-                            },
-                            container: {
-                                backgroundColor: '#fff',
-                                borderRadius: 12,
-                                paddingLeft: 10,
-                                paddingRight: 10,
-                                elevation: 5,
-                                shadowColor: '#000000',
-                                shadowOffset: { width: 0, height: 3 },
-                                shadowOpacity: 0.2,
-                                zIndex: 3,
-                            },
-                            powered: {
-                                display: 'none',
-                                height: 0
-                            },
-                            poweredContainer: {
-                                padding: 0,
-                                height: 0
-                            }
-                        }}
-
-                        nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
-                        GoogleReverseGeocodingQuery={{
-                            // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
-                        }}
-                        GooglePlacesSearchQuery={{
-                            // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
-                            rankby: 'distance',
-                            types: 'food',
-                        }}
-                        GooglePlacesDetailsQuery={{
-                            // available options for GooglePlacesDetails API : https://developers.google.com/places/web-service/details
-                            fields: 'formatted_address',
-                        }}
-
-                        filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
-                    />
-
-
-                    <GooglePlacesAutocomplete
-                        minLength={2} // minimum length of text to search
-                        autoFocus={false}
-                        fetchDetails={true}
-                        onPress={async (data, details = null) => { // 'details' is provided when fetchDetails = true
-                            let latLng = await getLatLngFromGoogle(data.place_id)
-                            setEndPoint(latLng)
-                        }}
-                        getDefaultValue={() => {
-                            return ''; // text input default value
-                        }}
-                        query={{
-                            // available options: https://developers.google.com/places/web-service/autocomplete
-                            key: 'AIzaSyCgQNpZtDRvveEeDUkVBOrzy-TcV1QWbMU',
-                            language: 'en', // language of the results
-                        }}
-                        styles={{
-                            description: {
-                                fontWeight: 'bold',
-                                color: '#000',
-                                fontSize: 16
-                            },
-                            textInputContainer: {
-                                width: constants.width - 60,
-                                backgroundColor: '#fff',
-                                borderTopWidth: 0,
-                                borderBottomWidth: 0
-                            },
-                            predefinedPlacesDescription: {
-                                color: '#1faadb',
-                            },
-                            listView: {
-                                backgroundColor: '#fff',
-                                width: constants.width - 60,
-                                padding: 5,
-                                height: '100%'
-                            },
-                            container: {
-                                backgroundColor: '#fff',
-                                borderRadius: 12,
-                                paddingLeft: 10,
-                                paddingRight: 10,
-                                elevation: 5,
-                                shadowColor: '#000000',
-                                shadowOffset: { width: 0, height: 3 },
-                                shadowOpacity: 0.2,
-                                zIndex: 3,
-                                marginTop: 80,
-                            },
-                            powered: {
-                                display: 'none',
-                                height: 0
-                            },
-                            poweredContainer: {
-                                padding: 0,
-                                height: 0
-                            }
-                        }}
-
-                        nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
-                        GoogleReverseGeocodingQuery={{
-                            // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
-                        }}
-                        GooglePlacesSearchQuery={{
-                            // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
-                            rankby: 'distance',
-                            types: 'food',
-                        }}
-                        GooglePlacesDetailsQuery={{
-                            // available options for GooglePlacesDetails API : https://developers.google.com/places/web-service/details
-                            fields: 'formatted_address',
-                        }}
-
-                        filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
-                    />
+                    <GoogleTextInput onPressEvent={async (data) => {
+                        let latLng = await getLatLngFromGoogle(data.place_id)
+                        setBeginPoint(latLng)
+                    }}
+                        margin={0} />
+                    <GoogleTextInput onPressEvent={async (data) => {
+                        let latLng = await getLatLngFromGoogle(data.place_id)
+                        setEndPoint(latLng)
+                    }}
+                        margin={80} />
                 </SearchBarWrapper>
                 <PrimaryButton
                     text="Find Route"
@@ -191,6 +48,6 @@ export default (props) => {
                     }}
                 />
             </View>
-        </Wrapper>
+        </Wrapper >
     )
 }
